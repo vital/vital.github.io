37eb47603f4d4b1d05f00b23aef3a5df5d61986a
Vital Batmanov <vital.batmanov@gmail.com>
2021-07-13 17:22:20 +0300

    3

---
+++ docs/w8ben/.prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 140,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  semi: true,
+  singleQuote: true,
+  jsxBracketSameLine: false,
+  bracketSpacing: true,
+  jsxSingleQuote: false,
+  arrowParens: 'avoid'
+}
